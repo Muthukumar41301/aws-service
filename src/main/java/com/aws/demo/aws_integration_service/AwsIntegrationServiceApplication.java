e28b49fe2eb55927bfb6d1a85fd3630d7ac1dbfa
IMPIGERTECH\muthukumar.munusamy <muthukumar.munumsamy@impigertech.com>
2025-07-16 15:24:27 +0530

    dockerfile and buildspec added

---
+++ src/main/java/com/aws/demo/aws_integration_service/AwsIntegrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aws.demo.aws_integration_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsIntegrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsIntegrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/demo/aws_integration_service/AwsIntegrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.demo.aws_integration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsIntegrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
